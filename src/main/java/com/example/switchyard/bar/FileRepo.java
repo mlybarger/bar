30216ad3f4f81c4d12a005c275a731ac68bb0575
mark <mlybarger@gmail.com>
2015-04-14 19:03:13 -0400

    changes for camel route.

---
+++ src/main/java/com/example/switchyard/bar/FileRepo.java
@@ -0,0 +1,18 @@
+package com.example.switchyard.bar;
+
+import java.io.File;
+
+import javax.inject.Named;
+
+import org.apache.camel.processor.idempotent.FileIdempotentRepository;
+import org.apache.camel.util.LRUCache;
+
+@Named("FileRepo")
+public class FileRepo extends FileIdempotentRepository  {
+	
+	public FileRepo() {
+		super();
+		setFileStore(new File("/tmp/FileRepo"));
+		setCache(new LRUCache<String,Object>(100000));		
+	}
+}
--- src/main/java/com/example/switchyard/bar/IFileRepo.java
+++
@@ -1,5 +0,0 @@
-package com.example.switchyard.bar;
-
-public interface IFileRepo {
-
-}
